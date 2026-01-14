a93944836cafb1c23a37a8557067862801d10c98
Seungwoo-Yoon <dotori000@snu.ac.kr>
2026-01-14 14:51:55 +0900

    install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+apt-get install gh
+
+git clone https://github.com/Seungwoo-Yoon/sam-3d-objects.git
+cd sam-3d-objects
+
+conda env create -f environments/default.yml
+conda activate sam3d-objects
+
+export PIP_EXTRA_INDEX_URL="https://pypi.ngc.nvidia.com https://download.pytorch.org/whl/cu121"
+pip install -e '.[dev]'
+pip install -e '.[p3d]'
+
+export PIP_FIND_LINKS="https://nvidia-kaolin.s3.us-east-2.amazonaws.com/torch-2.5.1_cu121.html"
+pip install -e '.[inference]'
+
+./patching/hydra
